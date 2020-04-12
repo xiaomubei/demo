03ed2b7d625ece1ac0b8ffab41803384e7c34d6b
mayong1314 <2772471646@qq.com>
2020-04-12 12:44:37 +0800

    spring boot 集成 jwt
    Signed-off-by: mayong1314 <2772471646@qq.com>

--- src/main/java/org/triber/demo/demo/api/loginUser/TScLoginUserController.java
+++ src/main/java/org/triber/demo/demo/api/loginUser/TScLoginUserController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.triber.demo.demo.common.R;
+import org.triber.demo.demo.common.RedisUtil;
 import org.triber.demo.demo.service.loginUser.ITScLoginUserService;
+
+import javax.annotation.Resource;
 
 /**
  * @Description:TODO(系统用户表接口api)
@@ -20,11 +23,22 @@ public class TScLoginUserController {
     @Autowired
     private ITScLoginUserService tScLoginUserService;
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    public static void main(String[] args) {
+
+    }
+
     @ApiOperation(value = "获取数据", httpMethod = "GET", notes = "获取分页数据")
     @ApiImplicitParam(name = "page", value = "页数", dataType = "int", paramType = "query")
     @RequestMapping("/page")
     public R selectPage() {
         Page page = tScLoginUserService.selectUserPage(null);
+        redisUtil.set("username", "mayong");
+        System.out.println("redis 获取：" + redisUtil.get("username"));
+        redisUtil.delete("username");
+        System.out.println("redis 删除key");
         return new R<>(page.getRecords(), page.getTotal());
     }
 }
---
+++ src/main/java/org/triber/demo/demo/common/JwtUtil.java
@@ -0,0 +1,8 @@
+package org.triber.demo.demo.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtUtil {
+
+}
