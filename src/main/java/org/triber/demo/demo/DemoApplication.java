edaaae6999773d61baec99ab3fd2ec627d0b6892
mayong <2772471646@qq.com>
2020-04-21 22:27:25 +0800

    加入spring client 集成

--- src/main/java/org/triber/demo/demo/DemoApplication.java
+++ src/main/java/org/triber/demo/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package org.triber.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoApplication {
 
     public static void main(String[] args) {
